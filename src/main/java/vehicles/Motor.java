7e99178ebf271ac5603bc810a57f21714fcae4a9
hadams0 <adamshannah1@icloud.com>
2023-07-20 10:11:26 +0100

    changes made to mvp code

--- src/main/java/vehicles/Motor.java
+++ src/main/java/vehicles/Motor.java
@@ -10,32 +10,26 @@ public class Motor implements IMotorised{
         this.fuel = fuel;
     }
 
-    @Override
     public void startEngine() {
-        System.out.println("engine started");
+        System.out.println("motor started");
     }
 
-    @Override
     public void stopEngine() {
-        System.out.println("engine stopped");
+        System.out.println("motor stopped");
     }
 
-    @Override
     public int getHp() {
         return this.hp;
     }
 
-    @Override
     public void setHp(int hp) {
         this.hp = hp;
     }
 
-    @Override
     public int getFuel() {
         return this.fuel;
     }
 
-    @Override
     public void setFuel(int fuel) {
         this.fuel = fuel;
     }
--- src/main/java/vehicles/water/Speedboat.java
+++ src/main/java/vehicles/water/Speedboat.java
@@ -6,61 +6,45 @@ import vehicles.Vehicle;
 
 public class Speedboat extends Vehicle implements IMotorised, IWaterVehicle {
 
-    private String hullType;
     private IMotorised motor;
+    private String hullType;
 
-    public Speedboat(float weight, int maxSpeed, IProduct baseProduct, IMotorised motor){
+    public Speedboat(float weight, int maxSpeed, IProduct baseProduct, IMotorised motor, String hullType){
         super(weight, maxSpeed, baseProduct);
-        this.hullType = hullType;
         this.motor = motor;
+        this.hullType = hullType;
     }
 
-    @Override
     public void startEngine() {
-        System.out.println("engine started");
+        this.motor.startEngine();
     }
 
-    @Override
     public void stopEngine() {
-        System.out.println("engine stopped");
+        this.motor.stopEngine();
     }
 
-    @Override
     public int getHp() {
         return this.motor.getHp();
     }
 
-    @Override
     public void setHp(int hp) {
         this.setHp(hp);
-
     }
 
-    @Override
     public int getFuel() {
         return this.motor.getFuel();
     }
 
-    @Override
     public void setFuel(int fuel) {
-        this.setFuel(fuel);
+        this.motor.setFuel(fuel);
     }
 
-    @Override
-    public String gethullType() {
-        return this.gethullType();
+    public String getHullType() {
+        return this.getHullType();
     }
 
-    @Override
     public void setHullType(String hullType) {
         this.setHullType(hullType);
     }
 
-    public IMotorised getMotor() {
-        return motor;
-    }
-
-    public void setMotor(IMotorised motor) {
-        this.motor = motor;
-    }
 }
